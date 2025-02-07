a6ce9d0b239343fca404753464d7e285c98a3ea6
jkl <john.k.lee@qq.com>
2025-02-07 17:11:20 +0800

    完善了壁纸切换

--- web/myscript.js
+++ web/myscript.js
@@ -7,23 +7,26 @@ function play() {
 function changeWallpaper() {
     var body = document.body;
     const wallpapers = [
-        "image/Background/wallpaer/wp (1).jpg",
-        "image/Background/wallpaer/wp (1).png",
-        "image/Background/wallpaer/wp (1).webp", 
-        "image/Background/wallpaer/wp (2).jpg",
-        "image/Background/wallpaer/wp (2).webp",
-        "image/Background/wallpaer/wp (3).jpg",
-        "image/Background/wallpaer/wp (3).webp",
-        "image/Background/wallpaer/wp (4).jpg",
-        "image/Background/wallpaer/wp (4).webp",
-        "image/Background/wallpaer/wp (5).jpg",
-        "image/Background/wallpaer/wp (6).jpg",
-        "image/Background/wallpaer/wp (7).jpg",
+        "image/Background/wallpaper/wp (1).jpg",
+        "image/Background/wallpaper/wp (1).png",
+        "image/Background/wallpaper/wp (1).webp", 
+        "image/Background/wallpaper/wp (2).jpg",
+        "image/Background/wallpaper/wp (2).webp",
+        "image/Background/wallpaper/wp (3).jpg",
+        "image/Background/wallpaper/wp (4).jpg",
+        "image/Background/wallpaper/wp (4).webp",
+        "image/Background/wallpaper/wp (5).jpg",
+        "image/Background/wallpaper/wp (6).jpg",
+        "image/Background/wallpaper/wp (7).jpg",
     ];
     var randomIndex = Math.floor(Math.random() * wallpapers.length);
     var i = 'url("' + wallpapers[randomIndex] + '")';
     body.style.backgroundImage = i;
     console.log(body.style.backgroundImage);
 }
-
+function changetest() {
+    var body = document.body;
+    body.style.backgroundImage = 'url("image/Background/wallpaer/wp (1).jpg")';
+    
+}
 
